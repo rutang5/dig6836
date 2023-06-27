53141eefa2c8dd8c64e70e1f5912556ec18fd3fb
Rudy McDaniel <rudy@ucf.edu>
2023-06-26 21:27:39 -0400

    randomQuoteGenerator

--- code-exercises/test.py
+++ code-exercises/test.py
@@ -1,3 +1,19 @@
+import random
+
 print("Hello, World!")
 
 def sayHi():
+    print("Hi!")
+
+def sayBye():
+    print("Bye!")
+
+
+def displayRandomQuote():
+    quotes = ["Quote 1", "Quote 2", "Quote 3"]
+    randomQuote = random.randint(0, len(quotes) - 1)
+    print(quotes[randomQuote])
+
+sayHi()
+displayRandomQuote()
+
