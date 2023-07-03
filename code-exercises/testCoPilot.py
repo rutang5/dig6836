05ea863e533d993a0b05f7257c2397705dc6e3b8
Rudy McDaniel <rudy@ucf.edu>
2023-07-03 18:24:09 -0400

    updates

---
+++ code-exercises/testCoPilot.py
@@ -0,0 +1,11 @@
+def averageList(list):
+    sum = 0
+    for i in list:
+        sum += i
+    return sum / len(list)
+
+nums = [1, 2, 3, 4, 5, 6, 7, 12, 15, 28]
+print(averageList(nums))
+
+# Path: dig6836\code-exercises\testCoPilot.py
+
